d09568eb582ce31d8708996c0a6f4a818dd9f254
leoldding <leo.ld.ding@gmail.com>
2022-09-30 12:52:04 -0400

    added postgres service

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"github.com/gin-gonic/gin"
+	_ "github.com/lib/pq"
 	"net/http"
 )
 
@@ -9,9 +12,25 @@ type Result struct {
 	Coin string `form:"coin" json:"coin" binding:"required"`
 }
 
+const (
+	host = "localhost"
+	port = 5432
+	user = "postgres"
+	pw   = "postgres_password"
+	db   = "postgres"
+)
+
 func main() {
 	router := gin.Default()
 	router.SetTrustedProxies([]string{":3000"})
+
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+"password=%s dbname=%s sslmode=disable", host, port, user, pw, db)
+
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
 
 	backend := router.Group("/backend")
 	{

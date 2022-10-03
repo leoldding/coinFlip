7f8fc001937a454454e378af6591e3fc00b5290a
leoldding <leo.ld.ding@gmail.com>
2022-10-03 11:32:02 -0400

    added percentage of heads

--- frontend/index.js
+++ frontend/index.js
@@ -2,6 +2,7 @@ flipCoin = document.getElementById("flipButton");
 flipResult = document.getElementById("flipResult");
 headCount = document.getElementById("headCount");
 tailCount = document.getElementById("tailCount");
+percentage = document.getElementById("percentage");
 resetCounts = document.getElementById("resetButton");
 
 flipCoin.addEventListener("click", flip);
@@ -30,6 +31,7 @@ function flip() {
             } else if(coinSide == "tail") {
                 tailCount.textContent = JSON.parse(data).value
             }
+            percentage.textContent = String(parseInt(headCount.textContent) / (parseInt(headCount.textContent) + parseInt(tailCount.textContent)) * 100) + "%";
         });
     }).catch((error) => {
         console.log(error)
@@ -44,6 +46,7 @@ function reset() {
     }).then(function (data) {
             headCount.textContent = "0";
             tailCount.textContent = "0";
+            percentage.textContent = "0%";
     }).catch((error) => {
         console.log(error)
     });
@@ -56,6 +59,12 @@ document.addEventListener("DOMContentLoaded", function() {
             response.text().then(function (data) {
                 headCount.textContent = JSON.parse(data).headNum;
                 tailCount.textContent = JSON.parse(data).tailNum;
+                if (JSON.parse(data).headNum == "0" && JSON.parse(data).tailNum == "0") {
+                    percentage.textContent = "0%"
+                }
+                else {
+                    percentage.textContent = String(parseInt(JSON.parse(data).headNum) / (parseInt(JSON.parse(data).headNum) + parseInt(JSON.parse(data).tailNum)) * 100) + "%";
+                }
             });
         }).catch((error) => {
             console.log(error)
